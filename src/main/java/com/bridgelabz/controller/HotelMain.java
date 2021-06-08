b2dfb6bf2a2b77479f2c22a8a2e3ac8cd94e00bb
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-08 13:39:47 +0530

    Welcome to Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/controller/HotelMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.controller;
+
+public class HotelMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to Hotel Reservation System");
+        System.out.println("Have a Safe Stay");
+    }
+}
